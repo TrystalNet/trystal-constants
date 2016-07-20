6a1d967b0bf97aab7a356373cb68bcea07266c1b
Terry O'Neill <terry.oneill@trystal.net>
2016-07-20 08:26:54 -0600

    npm link testing

--- index.d.ts
+++ index.d.ts
@@ -9,3 +9,6 @@ export enum Formats { UNKNOWN= 0, FMT2014 = 20140, FMT2014A= 20141, FMT2015= 201
 export enum ForegroundColors { black=0, blue=2, green, red, gray }
 export enum BackgroundColors { yellow=1, powderblue, palegreen, mistyrose, lightgray }
 
+export interface NPMLinkTest {
+  (name):string
+}
